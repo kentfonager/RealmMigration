cb8829a19bbcfb18af85318fa10fae5277a3a1dc
kentfonager <kent@netcoders.dk>
2018-01-31 12:24:04 +0100

    First commit

---
+++ RealmMigration/Models/TestModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Realms;
+
+namespace RealmMigration.Models
+{
+	public class TestModel : RealmObject
+	{
+		[PrimaryKey]
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Surname { get; set; }
+	}
+}
---
+++ RealmMigration/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Realms;
+
+namespace RealmMigration
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			// open the local realm 
+			var realm = Realm.GetInstance(new RealmConfiguration("test.realm")
+			{
+				ShouldDeleteIfMigrationNeeded = true,
+			});
+
+
+			BuildWebHost(args).Run();
+		}
+
+		public static IWebHost BuildWebHost(string[] args) =>
+			WebHost.CreateDefaultBuilder(args)
+				.UseStartup<Startup>()
+				.Build();
+	}
+}
